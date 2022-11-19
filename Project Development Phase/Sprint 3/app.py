d8d080321b86ed11ac6171126d126200022ca0fc
Dhivi12 <85785364+Dhivi12@users.noreply.github.com>
2022-11-19 09:43:30 +0530

    Sprint 3 completed

---
+++ Project Development Phase/Sprint 3/app.py
@@ -0,0 +1,130 @@
+"""# import libraries
+from flask import Flask, render_template, request
+from newsapi import NewsApiClient
+  
+# init flask app
+app = Flask(__name__)
+  
+# Init news api
+newsapi = NewsApiClient(api_key='1484019d541f47b09c62808ef06ca6df')
+
+# this function returns a JSON object
+top_headlines = newsapi.get_top_headlines(country="in", language="en")
+total_results = top_headlines['totalResults']
+  
+# the maximum value for page_size parameter is 100
+# so we need to keep it at max 100
+if total_results > 100:
+    total_results = 100
+  
+# fetch articles where no. of articles=total_results
+# this returns a list of articles
+all_headlines = newsapi.get_top_headlines(country="in",
+                                          language="en",
+                                          page_size=total_results)['articles']
+
+# this function will return sources and domains in str format
+# each of them separated by a comma
+def get_sources_and_domains():
+    all_sources = newsapi.get_sources()['sources']
+    sources = []
+    domains = []
+    for e in all_sources:
+        id = e['id']
+        domain = e['url'].replace("http://", "")
+        domain = domain.replace("https://", "")
+        domain = domain.replace("www.", "")
+        slash = domain.find('/')
+        if slash != -1:
+            domain = domain[:slash]
+        sources.append(id)
+        domains.append(domain)
+    sources = ", ".join(sources)
+    domains = ", ".join(domains)
+    return sources, domains
+  
+  
+# in post request
+sources, domains = get_sources_and_domains()
+related_news = newsapi.get_everything(q=keyword,
+                                      sources=sources,
+                                      domains=domains,
+                                      language='en',
+                                      sort_by='relevancy')
+no_of_articles = related_news['totalResults']
+  
+# the max number of article can be fetched is 100
+if no_of_articles > 100:
+    no_of_articles = 100
+all_articles = newsapi.get_everything(q=keyword,
+                                      sources=sources,
+                                      domains=domains,
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page_size=no_of_articles)['articles']
+  
+# we will send this list of articles to the html web page
+"""
+# import libraries
+from flask import Flask, render_template, request
+from newsapi import NewsApiClient
+  
+# init flask app
+app = Flask(__name__)
+  
+# Init news api 
+newsapi = NewsApiClient(api_key='70fdb9ba81ba40b6bda148e672898bd9')
+  
+# helper function
+def get_sources_and_domains():
+    all_sources = newsapi.get_sources()['sources']
+    sources = []
+    domains = []
+    for e in all_sources:
+        id = e['id']
+        domain = e['url'].replace("http://", "")
+        domain = domain.replace("https://", "")
+        domain = domain.replace("www.", "")
+        slash = domain.find('/')
+        if slash != -1:
+            domain = domain[:slash]
+        sources.append(id)
+        domains.append(domain)
+    sources = ", ".join(sources)
+    domains = ", ".join(domains)
+    return sources, domains
+  
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        sources, domains = get_sources_and_domains()
+        keyword = request.form["keyword"]
+        related_news = newsapi.get_everything(q=keyword,
+                                      sources=sources,
+                                      domains=domains,
+                                      language='en',
+                                      sort_by='relevancy')
+        no_of_articles = related_news['totalResults']
+        if no_of_articles > 100:
+            no_of_articles = 100
+        all_articles = newsapi.get_everything(q=keyword,
+                                      sources=sources,
+                                      domains=domains,
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page_size = no_of_articles)['articles']
+        return render_template("home.html", all_articles = all_articles, 
+                               keyword=keyword)
+    else:
+        top_headlines = newsapi.get_top_headlines(country="in", language="en")
+        total_results = top_headlines['totalResults']
+        if total_results > 100:
+            total_results = 100
+        all_headlines = newsapi.get_top_headlines(country="in",
+                                                     language="en", 
+                                                     page_size=total_results)['articles']
+        return render_template("home.html", all_headlines = all_headlines)
+    return render_template("home.html")
+  
+if __name__ == "__main__":
+    app.run(debug = True)
